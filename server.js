7d7810e85286dc15a181a2274981248dfac00cf7
Faisal <mr.fiesal@gmail.com>
2016-11-13 15:29:27 +0300

    add delete method

--- server.js
+++ server.js
@@ -46,6 +46,19 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if(!matchedTodo){
+        res.status(400).json({"error": "no todo item found with that id"});
+    }
+    else{
+        todos = _.without(todos, matchedTodo);
+        res.json(todos);        
+    }
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
