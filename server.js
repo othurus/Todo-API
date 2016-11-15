b028c1d6d27a3b371c47d1a5e2a9f9ac5561da94
Faisal <mr.fiesal@gmail.com>
2016-11-15 05:52:07 +0300

    Convert GET todos/:id to use sequelize

--- server.js
+++ server.js
@@ -37,15 +37,21 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+    db.todo.findById(todoId).then(function (todo) {
+        if (!!todo) res.json(todo.toJSON());
+        else res.status(404).send();
+    }, function (e) {
+        res.status(500).josn(e);
     });
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    }
-    else {
-        res.status(404).send();
-    }
+    //    var matchedTodo = _.findWhere(todos, {
+    //        id: todoId
+    //    });
+    //    if (matchedTodo) {
+    //        res.json(matchedTodo);
+    //    }
+    //    else {
+    //        res.status(404).send();
+    //    }
 });
 // POST /todos
 app.post('/todos', function (req, res) {
