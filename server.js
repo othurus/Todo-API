a9fab9f1b38e50393778b97b1ab6522fd6086b99
Faisal <mr.fiesal@gmail.com>
2016-11-17 02:14:21 +0300

    Add user model and post /users request

--- server.js
+++ server.js
@@ -84,7 +84,16 @@ app.post('/todos', function (req, res) {
     //    todos.push(body);
     //    res.json(body);
 });
-//DELETE /todos/:id
+//POST /users
+app.post('/users', function (req, res) {
+        var body = _.pick(req.body, 'email', 'password');
+        db.user.create(body).then(function (user) {
+            res.json(user.toJSON());
+        }, function (e) {
+            res.status(400).json(e);
+        });
+    })
+    //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     db.todo.destroy({
